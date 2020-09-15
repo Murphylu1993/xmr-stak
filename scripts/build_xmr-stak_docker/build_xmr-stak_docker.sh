e5a99c94d263145325c7edb2b9a3d1f3465ed5a2
Murphylu1993 <luqi_yewu@cmss.chinamobile.com>
2020-09-15 17:13:04 +0800

    Update build_xmr-stak_docker.sh

--- scripts/build_xmr-stak_docker/build_xmr-stak_docker.sh
+++ scripts/build_xmr-stak_docker/build_xmr-stak_docker.sh
@@ -20,7 +20,7 @@ fi
 ########################
 docker run --rm -it -v $PWD:/mnt ubuntu:17.04 /bin/bash -c "
 set -x ;
-echo \"deb http://old-releases.ubuntu.com/ubuntu disco main universe multiverse restricted\" >  /etc/apt/sources.list ;:
+echo \"deb http://old-releases.ubuntu.com/ubuntu disco main universe multiverse restricted\" >  /etc/apt/sources.list ;
 apt update -qq ;
 apt install -y -qq libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev ;
 cd /mnt/xmr-stak ;
